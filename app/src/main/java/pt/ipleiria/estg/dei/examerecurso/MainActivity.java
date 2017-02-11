2e3a403580c69552e41406bbae15f4927a262c54
João Mota <joao.lx@gmail.com>
2017-02-11 10:59:31 +0000

    Pergunta 3 concluida

--- app/src/main/java/pt/ipleiria/estg/dei/examerecurso/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/examerecurso/MainActivity.java
@@ -11,10 +11,14 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+import pt.ipleiria.estg.dei.examerecurso.colecoes.Fila;
 import pt.ipleiria.estg.dei.examerecurso.colecoes.Iterador;
+import pt.ipleiria.estg.dei.examerecurso.colecoes.Pilha;
+import pt.ipleiria.estg.dei.examerecurso.colecoes.TabelaHashPorListas;
 import pt.ipleiria.estg.dei.examerecurso.modelo.ModeloDados;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnAnagrama;
     private EditText editText1;
     private EditText editText2;
-    private TextView resAnagramas;
+    private TextView txtResAnagramas;
+    private TextView txtResCompressao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
         btnAnagrama = (Button) findViewById(R.id.btnAnagramas);
         editText1 = (EditText) findViewById(R.id.editText1);
         editText2 = (EditText) findViewById(R.id.editText2);
-        resAnagramas = (TextView) findViewById(R.id.textViewResultadoAnagrama);
+        txtResAnagramas = (TextView) findViewById(R.id.textViewResultadoAnagrama);
+        txtResCompressao = (TextView) findViewById(R.id.textViewResCompressao);
 
         LinkedList<String> listaTestes = new LinkedList<>();
         Iterador<String> it = ModeloDados.INSTANCIA.getListaExemplos();
@@ -53,8 +59,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void onComprimirClick(View view){
         String descomprimido = (String)spinner.getSelectedItem();
+        System.out.println("Descomprimido: " + descomprimido);
+        StringBuilder inversor = new StringBuilder(descomprimido);
+        String invertido = inversor.reverse().toString();
+        System.out.println("Invertido: " + invertido);
 
-        System.out.println("String a comprimir: " + descomprimido);
+        Fila<String> filaRes = new Fila<>();
+
+        Character anterior = null;
+        Character actual;
+        StringBuilder conjuntos = null;
+        for (int i = 0; i < invertido.length(); i++){
+            actual = invertido.charAt(i);
+            if(anterior == null || actual != anterior){
+                if(conjuntos != null){
+                    filaRes.inserir(conjuntos.toString());
+                }
+                conjuntos = new StringBuilder();
+                //conjuntos.append(actual);
+            }
+            conjuntos.append(actual);
+            anterior = actual;
+        }
+        filaRes.inserir(conjuntos.toString());
+
+
+        StringBuilder res = new StringBuilder();
+        while(!filaRes.isVazia()){
+            String conjuntoChars = filaRes.remover();
+            res.append(Integer.toString(conjuntoChars.length()));
+            res.append(conjuntoChars.charAt(0));
+        }
+
+        txtResCompressao.setText(res.toString());
+
     }
 
 }
